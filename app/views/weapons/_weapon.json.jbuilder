2049a233f66a14e5e1d5eabd97860d373fa5a820
Jeno Saghi <jeno.2010@yahoo.com>
2023-04-04 03:06:46 +0000

    Generated rails scaffolding

---
+++ app/views/weapons/_weapon.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! weapon, :id, :name, :description, :dmg_type, :dmg_dice, :int, :power_level, :created_at, :updated_at
+json.url weapon_url(weapon, format: :json)
---
+++ db/migrate/20230404030051_create_weapons.rb
@@ -0,0 +1,14 @@
+class CreateWeapons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :weapons do |t|
+      t.string :name
+      t.string :description
+      t.string :dmg_type
+      t.string :dmg_dice
+      t.string :int
+      t.int :power_level
+
+      t.timestamps
+    end
+  end
+end
