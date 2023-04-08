42dd8153940a6da860f03ef10b3c569751dd4d19
Jeno Saghi <jeno.2010@yahoo.com>
2023-04-08 04:00:45 +0000

    starting another branch because this one is fucked

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidPlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}))
 
 module.exports = environment
